7ca70c602676f783abf9d5322a889c09ef68a017
Re1ncarnat10 <128154276+Re1ncarnat10@users.noreply.github.com>
2024-05-06 01:39:22 +0200

    backend naprawiony (#2)

---
+++ mymovielist.client/src/Components/Footer.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <p>MyMovieList 2023 Prawa zastrze¿one</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ mymovielist.client/src/Components/Header.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <h1>Jest to strona g³ówna</h1>
+        </header>
+    );
+}
+
+export default Header;
